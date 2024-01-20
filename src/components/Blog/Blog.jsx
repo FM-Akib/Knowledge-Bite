d4b72ed2d0875289b084c07f3250cfd9d660ecee
FM-Akib <muntasirakib08@gmail.com>
2024-01-20 21:45:34 +0600

    Blog part done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,19 @@
 // import React from 'react';
 import './Blog.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+
 
 const Blog = (prop) => {
     const {img,title,profile,name,readTime}=prop.blog;
+    const token1 = prop.blog.tag[0];
+    const token2 = prop.blog.tag[1];
+
+
     return (
         <div className='Blog'>
             <img src={img} alt="blog"/>
+
             <div className='UserInfo'>
                 <div className='userIN'>
                 <img src={profile} alt="profile"/>
@@ -14,12 +22,16 @@ const Blog = (prop) => {
                     <p>13 January</p>
                 </div>
                 </div>
-                <div>
-                <p>{readTime}</p>
-                <button>B</button>
+                <div className='bookmark'>
+                <p>{readTime} min read</p>
+                <button className='bookmark-btn'><FontAwesomeIcon className='bookmark-icn' icon={faBookmark} /></button>
                 </div>
             </div>
+
+
             <h2 className='title'>{title}</h2>
+            <p className='tag'>#{token1}  #{token2}</p>
+            <button className='markRead-btn'>Mark As Read</button>
         </div>
     );
 };
