f6b8c7a0966fe70c701ec891d3ce1a4f0878bde2
FM-Akib <muntasirakib08@gmail.com>
2024-01-20 20:08:57 +0600

    Navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
 // import React from 'react';
 import logo from '../../assets/logo.png';
 import './Header.css';
+
+import user from '../../assets/user.png'
+
 const Header = () => {
     return (
-        <nav className='Navbar'>
-            <img src={logo} alt="logo"/>
+        <nav className='Nav'>
+            <div className='Navbar'>
+            <img className="logo" src={logo} alt="logo"/>
+            <div className='NavItemsWimage'>
             <div className="NavItems">
                 <a href="">Tophics</a>
                 <a href="">Write</a>
                 <a href="">Login</a>
+                
             </div>
+            <img className="user" src={user} alt="user"/>
+            </div>
+            </div>
+            <hr></hr>
         </nav>
     );
 };
